d1815e74c47ce1b251b62be17b7e6c1d9a3ebe72
daniellebrazeau <91274159+daniellebrazeau@users.noreply.github.com>
2022-01-30 14:21:39 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 5,
     center: { lat: 51.0547, lng: -87.31882 },
   });
   // Create an array of alphabetical characters used to label the markers.
